c99138dccd5346faa491c7596137a23876bf0f82
Khwilo Kabaka <kwhyzzy@gmail.com>
2015-04-25 20:52:56 +0300

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,8 +3,7 @@ class VisitorsController < ApplicationController
   def new 
 
     @owner = Owner.new
-    flash.now[:notice] = 'Welcome!'
-    flash.now[:alert] = 'My birthday is soon.'
+    
   end
 
 end
